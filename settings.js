cf97816b8b13c12a72c6d31f612f2340c620ebce
Tristan7122 <154232254+Tristan7122@users.noreply.github.com>
2024-09-05 20:33:20 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ const chalk = require('chalk')
 module.exports = {
   SESSION_ID: '',
   ALIVE_REACT: '👋',
-  ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
+  ALIVE_MESSAGE: `*Hello 😊👋@name [ And @gname @members Number of Members ] I am Alive Now !*
 
 ⏰ Now time is @time
 📅 Date : @date
@@ -19,39 +19,39 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
-  PREFIX: '.$&',
+  MONGODB_URL: 'mongodb+srv://herrison:wells@cluster0.tqhtuou.mongodb.net/?retryWrites=true&w=majority', 
+  PREFIX: '.',
   LANGUAGE: 'EN',
   PORT: 3000,
   BOT_NAME: '𝐐𝐔𝐄𝐄𝐍 𝐄𝐋𝐈𝐒𝐀',
-  OWNER_NAME: 'ᴍ ʀ  ɴ ɪ ᴍ ᴀ',
-  OWNER_NUMBER: '94715166712',
-  SUDO: '94715166712',
+  OWNER_NAME: 'Tristan',
+  OWNER_NUMBER: '27634624586',
+  SUDO: '27656319889',
   WORK_TYPE: 'public',
   WORK_MODE: 'group',
-  TIME_ZONE: 'Asia/Colombo',
-  CAPTION: '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴍᴅ',
-  STICKER_AUTHOR: 'Qᴜᴇᴇɴ ᴇʟɪꜱᴀ',
-  STICKER_PACK: 'ᴍʀ ɴɪᴍᴀ ᴏꜰᴄ',
+  TIME_ZONE: 'Africa/Johannesburg',
+  CAPTION: '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ',
+  STICKER_AUTHOR: 'Tristan®',
+  STICKER_PACK: 'TRISTAN-BOT',
   REMOVE_BG_APIKEY: '',
-  MAXIMUM_FILESIZE: 90,
-  OWNER_R_EMOJI: '👽',
+  MAXIMUM_FILESIZE: 900,
+  OWNER_R_EMOJI: '✨',
   ALIVE_AS_GIF: true,
   AUTO_VOICE: false,
   INBOX_BLOCK: false,
   OWNER_REACT: true,
-  AUTO_REACT: true,
+  AUTO_REACT: false,
   ONLY_PM_GROUP_MSG_SEND: false,
-  AUTO_SEEN_STATUS: true,
+  AUTO_SEEN_STATUS: false,
   BOT_OFFLINE: false,
   READ_MESSAGE: false,
   ANTI_MSG_SPAM: false,
   ANTI_BAD_WORD: false,
-  ANTI_VIEW_ONES: true,
+  ANTI_VIEW_ONES: false,
   ANTI_DELETE: true,
-  BANED_USER_MSG_SEND: false,
-  BLOCK_CMD_MSG_SEND: false,
-  INBOX_BLOCK_MSG_SEND: false,
+  BANED_USER_MSG_SEND: true,
+  BLOCK_CMD_MSG_SEND: true,
+  INBOX_BLOCK_MSG_SEND: true,
   CMD_CORRECT: true,
   ANTI_CHANNEL_FORWARD: false,
   ANTI_BOT: false,
@@ -63,14 +63,14 @@ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   MSG: {
     ONLY_GROUP_MSG: 'Bot only Can Use Groups!',
     ONLY_PM_MSG: 'Bot only Can Use Private Chats!',
-    BAD_DETECT_MSG: '@user Bad Words Are Disable by Owner!',
-    BANED_MSG: "@user You Can't use bot, You are baned by owner!",
-    GROUP_LEAVE: '👋 Bye @groupName I am Going.',
+    BAD_DETECT_MSG: '@user Bad Words Are Disable by my Owner!',
+    BANED_MSG: "@user You Can't use bot, *You are banned by my owner,* contact:+27634624586!",
+    GROUP_LEAVE: 'BYE👋 @groupName I am Going.',
     INBOX_BLOCK:
-      '❌ My owner has activated auto inbox blocking. So you will be auto blocked after 8 seconds'
+      '❌ *you texted me without My owner(+27634624586) permission,* *So you will be auto blocked after 8 seconds😊BYE👋*'
   },
   BOT_RULES:
-    "*⚖️ 𝚀𝚄𝙴𝙴𝙽 𝙴𝙻𝙸𝚂𝙰 𝙱𝙾𝚃 𝚁𝚄𝙻𝙴𝚂 ⚖️*\n\n☫ Don't spam messages.\n☫ Don't add groups without asking owner.\n"
+    "*⚖️ 𝚀𝚄𝙴𝙴𝙽 𝙴𝙻𝙸𝚂𝙰 𝙱𝙾𝚃 𝚁𝚄𝙻𝙴𝚂 ⚖️*\n\n☫ Don't spam messages.\n☫ Don't add me to groups without asking my owner.\n"
 }
 
 let file = require.resolve(__filename)
